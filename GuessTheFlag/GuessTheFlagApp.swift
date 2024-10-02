5a282bf5fd829b7728ad19b752db989ffba157e4
Meet Mistry <meet.m@simformsolutions.com>
2024-10-02 13:08:00 +0530

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Meet Mistry on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
