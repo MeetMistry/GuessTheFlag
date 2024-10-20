eef1151511b45e5560f0b1e0f99ce846eb5fa039
Meet Mistry <meet.m@simformsolutions.com>
2024-10-20 12:10:57 +0530

    Added TODO

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+// TODO:
+// When you tap a flag, make it spin around 360 degrees on the Y axis.
+// Make the other two buttons fade out to 25% opacity.
+// Add a third effect of your choosing to the two flags the user didn’t choose – maybe make them scale down? Or flip in a different direction? Experiment!
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
